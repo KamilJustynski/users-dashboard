b3bfa55d6d1a72bacff7624084a9020c89499330
Kamil Justynski <kamil.justynski1998@gmail.com>
2024-09-05 21:21:16 +0200

    added react router and few modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import { Home } from "./components/Home";
+import { Users } from "./components/Users";
 import { NavbarTop } from "./components/NavbarTop";
 import { NavbarLeft } from "./components/NavbarLeft";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./components/Dashboard";
 export const App = () => {
   return (
-    <div className="flex flex-col">
-      <div className="w-screen h-[100px] z-10">
-        <NavbarTop />
+    <BrowserRouter>
+      <div className="flex flex-col">
+        <div className="w-screen h-[100px] z-10">
+          <NavbarTop />
+        </div>
+        <div className="flex">
+          <NavbarLeft />
+          <Routes>
+            <Route path="/users" element={<Users />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
       </div>
-      <div className="flex">
-        <NavbarLeft />
-        <Home />
-      </div>
-    </div>
+    </BrowserRouter>
   );
 };
